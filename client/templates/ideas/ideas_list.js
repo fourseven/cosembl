68d611e509c267c7123acbca38a435e04c70de79
Sean Arnold <sean.arnold@abletech.co.nz>
2015-04-12 15:19:59 +1200

    more ideas page

--- client/templates/ideas/ideas_list.js
+++ client/templates/ideas/ideas_list.js
@@ -39,3 +39,13 @@ Template.ideasList.rendered = function () {
     }
   }
 }
+
+Template.ideasList.helpers({
+  activeRouteClass: function(/* route names */) {
+    var args = Array.prototype.slice.call(arguments, 0); args.pop();
+    var active = _.any(args, function(name) {
+      return Router.current() && Router.current().route.getName() === name
+    });
+    return active && 'selected';
+  }
+});
